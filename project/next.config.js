0f8c3ee8822112c2b45a10da0bb7b02242f23973
Your GitHub Username <your.email@example.com>
2025-03-22 01:17:51 +0530

    final commit

--- project/next.config.js
+++ project/next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/singh-portfolio/',
+  basePath: process.env.NODE_ENV === 'production' ? '/singh-portfolio' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
